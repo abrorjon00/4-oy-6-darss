5c9cc567a9e2111ef73c43b06a99b0317b56ab27
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-10-01 20:41:57 +0300

    dars

---
+++ app.js/index.js
@@ -0,0 +1,172 @@
+// 1-misol
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+    
+// }
+// 2-misol
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+// 3-misol
+
+// let num = +prompt("Soni kiriritng...");
+// for ( let i = 1; i <= num; i++) {
+//     console.log(i);
+//     }
+
+// 4-misol
+
+// for (let i = 0; i <= 10; i += 2) {
+//    console.log(i);
+   
+// }
+
+// 5-misol
+// for (let i = 1; i <= 15; i += 2) {
+//    console.log(i);
+   
+// }
+// 6-misol
+//  let a = +prompt("Sonni kiriting...")
+//  let b = +prompt("Sonni kiriting...")
+//  let c = +prompt("Sonni kiriting...")
+//  let d = +prompt("Sonni kiriting...")
+//  let e = +prompt("Sonni kiriting...")
+//  console.log(a,b,c,d,e);
+// 7-misol
+
+// for (let i = 15; i >= 5; i--) {
+//    console.log(i);
+//    }
+// 8-misol
+
+// let a = +prompt("Sonni kiriting...");
+// for (let i = 1; i <= a; i++) {
+// if (i % a == 0) {
+//     console.log(i);
+    
+// }
+// }
+
+// 9-misol
+// let a = +prompt("1-sonni kiriting...")
+// let b = +prompt("2-sonni kiriting...")
+// let c = +prompt("3-sonni kiriting...")
+// let d = +prompt("4-sonni kiriting...")
+// let e = +prompt("5-sonni kiriting...")
+// let f = +prompt("6-sonni kiriting...")
+// let g = +prompt("7-sonni kiriting...")
+// let h = +prompt("8-sonni kiriting...")
+// let i = +prompt("9-sonni kiriting...")
+// let j = +prompt("10-sonni kiriting...")
+
+// console.log(a+b+c+d+e+f+g+h+i+j);
+
+
+// 10-misol
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 5 == 0 ) {
+//         console.log(i);
+        
+//     }
+// }
+
+// 11-misol
+// for ( let i = 100; i >= 1; i--) {
+//     console.log(i);
+    
+// }
+
+// 12-misol
+// let res = 0;
+// for ( let i = 1; i <= 100; i++) {
+//      res = res + i
+        
+//     }
+//     console.log(res);
+
+// 13-misol
+
+// let num1 = +prompt("1-sonni kiriting...")
+// let num2 = +prompt("2-sonni kiriting...")
+
+// for ( let i = num1; i <= num2; i++) {
+//     console.log(i);
+    
+// }
+
+// 15-misol
+
+// for ( let i = 1; i <= 20; i++) {
+//     console.log(i**2);
+    
+// }
+
+// 16-misol
+
+// for ( let i = 1; i <= 100; i++) {
+//     if (i % 7 == 0) {
+//         console.log(i);
+        
+//     }
+// }
+
+// 17-misol
+// let res = 1;
+// for (let i = 1; i <= 10; i++) {
+// res = res * i
+// }
+// console.log(res);
+
+// 19-misol
+// let sum = 0;
+// for ( let i = 1; i <= 50; i++) {
+//     if ( i % 2 == 0) {
+//         sum = sum + i
+//     }
+// }
+
+// console.log(sum);
+
+// 20-misol
+// let coun = 0;
+// let num1 = +prompt("1-sonni kiriting...")
+// let num2 = +prompt("2-sonni kiriting...")
+
+// for (let i = num1; i <= num2; i++) {
+//     if (i % 2 == 0)  {
+//         console.log(i);
+        
+//     }
+// }
+
+
+// 21-misol
+// let res = 0;
+// for ( let i = 1; i <= 100; i++) {
+// if (i % 2 == 1) {
+//     res = res + i
+// }
+// }
+// console.log(res);
+
+// 24-misol
+
+// for ( let i = 1; i <= 30; i++) {
+//     if (i % 4 == 0 && i % 6 == 0) {
+//         console.log(i);
+        
+//     }
+// }
+
+// 25-misol
+    // let a = +prompt("1-sonni kiriting...")
+    // let b = +prompt("2-sonni kiriting...")
+
+    // for ( let i = a; i <= b; i++) {
+    //     if ( i % 2 == 1) {
+    //         console.log(i);
+            
+    //     }
+    // }
